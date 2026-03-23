660ee065cdfed925456b74cef0ab035e461e1775
Pawel-Tomczyk <adrestomczyka@gmail.com>
2026-03-23 19:35:40 +0100

    Update V1.0.1

--- src/uartPacket.cpp
+++ src/uartPacket.cpp
@@ -11,16 +11,22 @@ uint8_t calculateChecksum(UartPacket *cmd)
     }
     return checksum;
 }
+
+uint8_t calculateChecksum(uint8_t *data, size_t len) {
+    uint8_t checksum = 0;
+    // Zaczynamy od indeksu 2, żeby pominąć bajty startowe 0xAB 0xCD
+    for (size_t i = 2; i < len; i++) {
+        checksum ^= data[i];
+    }
+    return checksum;
+}
+
 unsigned int makePacket(UartPacket *cmd, uint8_t *packet)
 {
     packet[0] = START_BYTE_1;
     packet[1] = START_BYTE_2;
     packet[2] = cmd->id;
     packet[3] = cmd->length;
-    if (cmd->length > 255)
-    {
-        return 0; // error: length exceeds maximum allowed value
-    }
 
     for (int i = 0; i < cmd->length; i++)
     {
@@ -30,35 +36,37 @@ unsigned int makePacket(UartPacket *cmd, uint8_t *packet)
     return 5 + cmd->length; // total packet size is 5 bytes (2 start bytes, 1 id byte, 1 length byte, 1 checksum byte) + the length of the data
 }
 
+
+
 /*
     changes an int32_t to a byte array and vice versa, and the same for int16_t
     this is needed because the data is sent as bytes over UART, but we want to work with integers in the code
     ensures that the data is sent in big-endian format (most significant byte first)
-*/
-void int32ToBytes(int32_t value, uint8_t *bytes, uint8_t startIndex)
-{
-    bytes[startIndex + 0] = (value >> 24) & 0xFF;
-    bytes[startIndex + 1] = (value >> 16) & 0xFF;
-    bytes[startIndex + 2] = (value >> 8) & 0xFF;
-    bytes[startIndex + 3] = value & 0xFF;
-}
+// */
+// void int32ToBytes(int32_t value, uint8_t *bytes, uint8_t startIndex)
+// {
+//     bytes[startIndex + 0] = (value >> 24) & 0xFF;
+//     bytes[startIndex + 1] = (value >> 16) & 0xFF;
+//     bytes[startIndex + 2] = (value >> 8) & 0xFF;
+//     bytes[startIndex + 3] = value & 0xFF;
+// }
 
-int32_t bytesToInt32(uint8_t *bytes, uint8_t startIndex)
-{
-    return (uint32_t(bytes[startIndex + 0]) << 24) |
-           (uint32_t(bytes[startIndex + 1]) << 16) |
-           (uint32_t(bytes[startIndex + 2]) << 8) |
-           uint32_t(bytes[startIndex + 3]);
-}
+// int32_t bytesToInt32(uint8_t *bytes, uint8_t startIndex)
+// {
+//     return (uint32_t(bytes[startIndex + 0]) << 24) |
+//            (uint32_t(bytes[startIndex + 1]) << 16) |
+//            (uint32_t(bytes[startIndex + 2]) << 8) |
+//            uint32_t(bytes[startIndex + 3]);
+// }
 
-void int16ToBytes(int16_t value, uint8_t *bytes, uint8_t startIndex)
-{
-    bytes[startIndex + 0] = (value >> 8) & 0xFF;
-    bytes[startIndex + 1] = value & 0xFF;
-}
+// void int16ToBytes(int16_t value, uint8_t *bytes, uint8_t startIndex)
+// {
+//     bytes[startIndex + 0] = (value >> 8) & 0xFF;
+//     bytes[startIndex + 1] = value & 0xFF;
+// }
 
-int16_t bytesToInt16(uint8_t *bytes, uint8_t startIndex)
-{
-    return (uint16_t(bytes[startIndex + 0]) << 8) |
-           uint16_t(bytes[startIndex + 1]);
-}
+// int16_t bytesToInt16(uint8_t *bytes, uint8_t startIndex)
+// {
+//     return (uint16_t(bytes[startIndex + 0]) << 8) |
+//            uint16_t(bytes[startIndex + 1]);
+// }
--- src/uartPacket.hpp
+++ src/uartPacket.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdlib>
 
 
 #define START_BYTE_1 0xAB // start byte, used to identify the beginning of a packet, helps to synchronize the communication and avoid reading garbage data
 #define START_BYTE_2 0xCD // second start byte, used to further ensure that we are reading a valid packet, reduces the chances of false positives when looking for the start of a packet
 
-struct UartPacket
+struct __attribute__((packed)) UartPacket
 {
     uint8_t id;  // command id, used to identify the type of command
     uint8_t length;    // length of the data, used to know how many bytes to read for the data
@@ -20,14 +21,53 @@ struct UartPacket
     if the calculated checksum matches the checksum byte in the packet, then the data is considered valid
 */
 uint8_t calculateChecksum(UartPacket *cmd);
+uint8_t calculateChecksum(uint8_t *data, size_t len);
+
 unsigned int makePacket(UartPacket *cmd, uint8_t *packet);
 
+template <typename T>
+unsigned int makePacket(const T& dataStruct, uint8_t *outputBuffer) {
+    size_t dataSize = sizeof(T);
+    
+    // Zabezpieczenie rozmiaru (ID i LEN to po 1 bajcie)
+    if (dataSize > 255) return 0; 
+
+    outputBuffer[0] = START_BYTE_1;
+    outputBuffer[1] = START_BYTE_2;
+    outputBuffer[2] = T::id;
+    outputBuffer[3] = (uint8_t)dataSize;
+
+    // Kopiujemy zawartość struktury
+    memcpy(&outputBuffer[4], &dataStruct, dataSize);
+
+    // Obliczamy checksum dla całości (od początku do końca danych)
+    uint8_t crc = calculateChecksum(outputBuffer, 4 + dataSize);
+    outputBuffer[4 + dataSize] = crc;
+
+    // Zwracamy całkowitą długość ramki do wysłania
+    return 5 + dataSize;
+}
 /*
+    !!! NOT TRUE !!! redacted !!!
     changes an int32_t to a byte array and vice versa, and the same for int16_t
     this is needed because the data is sent as bytes over UART, but we want to work with integers in the code
     ensures that the data is sent in big-endian format (most significant byte first)
 */
-void int32ToBytes(int32_t value, uint8_t *bytes, uint8_t startIndex = 0);
-int32_t bytesToInt32(uint8_t *bytes, uint8_t startIndex = 0);
-void int16ToBytes(int16_t value, uint8_t *bytes, uint8_t startIndex = 0);
-int16_t bytesToInt16(uint8_t *bytes, uint8_t startIndex = 0);
+// void int32ToBytes(int32_t value, uint8_t *bytes, uint8_t startIndex = 0);
+// int32_t bytesToInt32(uint8_t *bytes, uint8_t startIndex = 0);
+// void int16ToBytes(int16_t value, uint8_t *bytes, uint8_t startIndex = 0);
+// int16_t bytesToInt16(uint8_t *bytes, uint8_t startIndex = 0);
+
+namespace Packets {
+    // Every template HAVE TO contain 'static const uint8_t id'
+    struct __attribute__((packed)) Heartbeat {
+        static const uint8_t id = 0x00;
+        uint32_t uptime_ms;
+        uint8_t status_code;
+    };
+
+    struct __attribute__((packed)) MsgTest {
+        static const uint8_t id = 0x01;
+        char message[20];
+    };
+};
